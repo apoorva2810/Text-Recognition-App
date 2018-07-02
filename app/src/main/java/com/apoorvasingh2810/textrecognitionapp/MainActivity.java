58abaf8d221255619e9994ba6b95ac13b8bca8b2
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-03 02:42:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/apoorvasingh2810/textrecognitionapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.apoorvasingh2810.textrecognitionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apoorvasingh2810.textrecognitionapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
