37fd0bb3ed2fa0706656ed7dca7b0f361431882b
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-03 17:16:53 +0530

    Snap Picture feature -added

--- app/src/main/java/com/apoorvasingh2810/textrecognitionapp/MainActivity.java
+++ app/src/main/java/com/apoorvasingh2810/textrecognitionapp/MainActivity.java
@@ -1,17 +1,67 @@
 package com.apoorvasingh2810.textrecognitionapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.apoorvasingh2810.textrecognitionapp.R;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button snapBtn;
+    private Button detectBtn;
+    private ImageView imageView;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        snapBtn=(Button)findViewById(R.id.snapBtn);
+        detectBtn=(Button)findViewById(R.id.detectBtn);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        txtView=(TextView)findViewById(R.id.txtView);
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakePictureIntent();
+            }
+        });
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
